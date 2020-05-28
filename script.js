39ca28115f44d4129c7b58457304955414d7bed1
Vinay C <65414132+VnyC@users.noreply.github.com>
2020-05-29 00:02:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var css= document.querySelector("h3");
+var c1= document.querySelector(".color1");
+var c2 = document.querySelector(".color2");
+var body= document.getElementById("gradient");
+
+function givecolor(){
+		body.style.background="linear-gradient( to right, "+ c1.value+" , "+ c2.value+")";
+		css.textContent= body.style.background +";";
+}
+
+
+c1.addEventListener("input", givecolor);
+
+c2.addEventListener("input", givecolor);
+
+
+
